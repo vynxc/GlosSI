9017159d039941dc01c20a9c57332b7c28f5502d
Maiko Tan <maiko.tan.coding@gmail.com>
2024-06-11 10:59:37 +0800

    ci: fix -o options

--- download_release_deps.ps1
+++ download_release_deps.ps1
@@ -1,3 +1,3 @@
-Invoke-WebRequest -o ViGEmBusSetup_x64.exe https://github.com/nefarius/ViGEmBus/releases/download/v1.22.0/ViGEmBus_1.22.0_x64_x86_arm64.exe
-Invoke-WebRequest -o HidHideSetup.exe https://github.com/nefarius/HidHide/releases/download/v1.4.192.0/HidHide_1.4.192_x64.exe
-Invoke-WebRequest -o vc_redist.x64.exe https://aka.ms/vs/16/release/vc_redist.x64.exe
+Invoke-WebRequest -OutFile ViGEmBusSetup_x64.exe https://github.com/nefarius/ViGEmBus/releases/download/v1.22.0/ViGEmBus_1.22.0_x64_x86_arm64.exe
+Invoke-WebRequest -OutFile HidHideSetup.exe https://github.com/nefarius/HidHide/releases/download/v1.4.192.0/HidHide_1.4.192_x64.exe
+Invoke-WebRequest -OutFile vc_redist.x64.exe https://aka.ms/vs/16/release/vc_redist.x64.exe
